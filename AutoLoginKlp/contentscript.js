f939ea6b756aa4e1fe4b8c2978474e01a0d02b94
Stef Käser <info.stef.kaeser@gmail.com>
2018-03-23 14:08:41 +1100

    updated for swissId

--- AutoLoginKlp/contentscript.js
+++ AutoLoginKlp/contentscript.js
@@ -1,65 +1,137 @@
+var initAutoLoginScript = function() {
+  chrome.storage.sync.get(
+    {
+      fillPw: true,
+      password: "##userpw1",
+      fillSmsCode: true,
+      autoSwissIdCode: true
+    },
+    function(options) {
+      console.log("KLP Auto Login Script loaded with options: ", options);
 
-var initAutoLoginScript = function () {
-	chrome.storage.sync.get({
-    fillPw: true, password: '##userpw1', fillSmsCode: true
-  }, function (options) {
-		console.log('KLP Auto Login Script loaded with options: ', options);
-		autoPassword(options);
-		autoSmsCode(options);
-	});
-}
+      var checkSite = function(options) {
+        var oneActionExecuted =
+          autoPassword(options) ||
+          autoPasswordSwissId(options) ||
+          autoSmsCode(options) ||
+          autoSwissIdCode(options) ||
+          checkAtuoRedirectToSwissId();
+
+        if (oneActionExecuted) {
+          console.log("stop timer as cause one action was executed");
+        } else {
+          setTimeout(function() {
+            checkSite(options);
+          }, 1000);
+        }
+      };
+
+      checkSite(options);
+    }
+  );
+};
 // check is there is an error on the site, which indicates that the last input was wrong
 // so we stop our scripts to prevent an endless loop
-var noErrorWithLastInput = function () {
-	var errorElement = document.getElementById('err_global');
-	return (errorElement && errorElement.innerHTML.trim());
-}
-
-
-var autoPassword = function (options) {
-	if (!options.fillPw) {
-		return;
-	}
-	var emailInput = document.getElementById('isiwebuserid');
-
-	if (emailInput && !noErrorWithLastInput()) {
-		emailInput.addEventListener('blur', function () {
-			var enteredEmail = emailInput.value;
-			if (enteredEmail && /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(enteredEmail)) {
-				var calculatedPw = calculatePw(enteredEmail, options);
-				document.getElementById('isiwebpasswd').value = calculatedPw;
-				console.log('password set to ' + calculatedPw);
-				var loginButton = document.getElementById('actionLogin');
-				loginButton.click();
-			}
-		});
-	}
-
-	function calculatePw(enteredEmail, options) {
-		if (options.password == "##userpw") {
-			return enteredEmail;
-		} else if (options.password == "##userpw1") {
-			return enteredEmail + '1';
-		}
-		return options.password;
-	}
+var noErrorWithLastInput = function() {
+  var errorElement = document.getElementById("err_global");
+  return errorElement && errorElement.innerHTML.trim();
 };
 
-var autoSmsCode = function (options) {
-	if (!options.fillSmsCode) {
-		return;
-	}
-	var twoFactorCode = document.getElementById('twoFactorCode');
-	var correctCode = document.getElementById('correctCode');
+var autoPassword = function(options) {
+  if (!options.fillPw) {
+    return;
+  }
+  var emailInput = document.getElementById("isiwebuserid");
 
+  if (emailInput && !noErrorWithLastInput()) {
+    emailInput.addEventListener("blur", function() {
+      var enteredEmail = emailInput.value;
+      if (
+        enteredEmail &&
+        /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(enteredEmail)
+      ) {
+        var calculatedPw = calculatePw(enteredEmail, options);
+        document.getElementById("isiwebpasswd").value = calculatedPw;
+        console.log("password set to " + calculatedPw);
+        var loginButton = document.getElementById("actionLogin");
+        loginButton.click();
+        return true;
+      }
+    });
+  }
+};
 
-	if (twoFactorCode && correctCode && !noErrorWithLastInput()) {
-		var correctCodeValue = correctCode.innerText;
-		twoFactorCode.value = correctCodeValue;
-		console.log('Two factor code set to ' + correctCodeValue);
-		var continueButton = document.getElementsByName('continue')[0];
-		continueButton.click();
-	}
+var autoPasswordSwissId = function(options) {
+  if (!options.fillPw) {
+    return;
+  }
+
+  var emailInput = document.getElementById("callback_3");
+
+  if (emailInput && emailInput.innerText) {
+    var enteredEmail = emailInput.innerText;
+    if (
+      enteredEmail &&
+      /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(enteredEmail)
+    ) {
+      var calculatedPw = calculatePw(enteredEmail, options);
+      document.getElementById("idToken5").value = calculatedPw;
+      console.log("password set to " + calculatedPw);
+      var loginButton = document.getElementById("idToken6_0");
+      loginButton.click();
+      return true;
+    }
+  }
+};
+
+function calculatePw(enteredEmail, options) {
+  if (options.password == "##userpw") {
+    return enteredEmail;
+  } else if (options.password == "##userpw1") {
+    return enteredEmail + "1";
+  }
+  return options.password;
+}
+
+var autoSmsCode = function(options) {
+  if (!options.fillSmsCode) {
+    return;
+  }
+  var twoFactorCode = document.getElementById("twoFactorCode");
+  var correctCode = document.getElementById("correctCode");
+
+  if (twoFactorCode && correctCode && !noErrorWithLastInput()) {
+    var correctCodeValue = correctCode.innerText;
+    twoFactorCode.value = correctCodeValue;
+    console.log("Two factor code set to " + correctCodeValue);
+    var continueButton = document.getElementsByName("continue")[0];
+    continueButton.click();
+    return true;
+  }
+};
+
+var autoSwissIdCode = function(options) {
+  if (!options.autoSwissIdCode) {
+    return;
+  }
+  var idToken4 = document.getElementById("idToken4");
+
+  if (idToken4 && idToken4.type != "email" && !noErrorWithLastInput()) {
+    idToken4.value = "00000";
+    console.log("swissId code set to 00000");
+    var continueButton = document.getElementById("idToken8_0");
+    continueButton.click();
+    return true;
+  }
+};
+
+var checkAtuoRedirectToSwissId = function() {
+  var redirectButton = document.getElementById("externalIDP");
+  if (redirectButton) {
+    console.log("auto redirect to swissId");
+    redirectButton.click();
+    return true;
+  }
 };
 
 initAutoLoginScript();
--- AutoLoginKlp/options.js
+++ AutoLoginKlp/options.js
@@ -1,69 +1,80 @@
 // Saves options to chrome.storage
 function save_options() {
-  var fillPw = document.getElementById('chkFillPassword').checked;
+  var fillPw = document.getElementById("chkFillPassword").checked;
   var options = { fillPw: fillPw };
   if (fillPw) {
-    if (document.getElementById('userpw').checked) {
+    if (document.getElementById("userpw").checked) {
       options.password = "##userpw";
-    } else if (document.getElementById('userpw1').checked) {
+    } else if (document.getElementById("userpw1").checked) {
       options.password = "##userpw1";
-    } else if (document.getElementById('otherpw').checked) {
-      options.password = document.getElementById('otherpwText').value;
+    } else if (document.getElementById("otherpw").checked) {
+      options.password = document.getElementById("otherpwText").value;
     }
   }
-  options.fillSmsCode = document.getElementById('chkFillSmsCode').checked;
+  options.fillSmsCode = document.getElementById("chkFillSmsCode").checked;
+  options.autoSwissIdCode = document.getElementById("chkSwissIdCode").checked;
 
-  chrome.storage.sync.set(options, function () {
+  chrome.storage.sync.set(options, function() {
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved!';
-    setTimeout(function () {
-      status.textContent = '';
+    var status = document.getElementById("status");
+    status.textContent = "Options saved!";
+    setTimeout(function() {
+      status.textContent = "";
     }, 1500);
   });
 }
-
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    fillPw: true, password: '##userpw1', fillSmsCode: true
-  },
-    function (items) {
-      document.getElementById('chkFillSmsCode').checked = items.fillPw;
-      document.getElementById('chkFillPassword').checked = items.fillSmsCode;
+  chrome.storage.sync.get(
+    {
+      fillPw: true,
+      password: "##userpw1",
+      fillSmsCode: true
+    },
+    function(items) {
+      document.getElementById("chkFillSmsCode").checked = items.fillPw;
+      document.getElementById("chkFillPassword").checked = items.fillSmsCode;
+      document.getElementById("chkSwissIdCode").checked = items.autoSwissIdCode;
       checkPwVisibility();
       if (items.password == "##userpw") {
-        document.getElementById('userpw').checked = true;
+        document.getElementById("userpw").checked = true;
       } else if (items.password == "##userpw1") {
-        document.getElementById('userpw1').checked = true;
+        document.getElementById("userpw1").checked = true;
       } else {
-        document.getElementById('otherpw').checked = true;
-        document.getElementById('otherpwText').value = items.password;
+        document.getElementById("otherpw").checked = true;
+        document.getElementById("otherpwText").value = items.password;
       }
-    });
+    }
+  );
 }
 
 function checkPwVisibility() {
-  if (document.getElementById('chkFillPassword').checked) {
-    document.getElementById('fillPwOptions').style.display = '';
+  if (document.getElementById("chkFillPassword").checked) {
+    document.getElementById("fillPwOptions").style.display = "";
   } else {
-    document.getElementById('fillPwOptions').style.display = 'none';
+    document.getElementById("fillPwOptions").style.display = "none";
   }
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
-document.getElementById('chkFillPassword').addEventListener('click', checkPwVisibility);
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
+document
+  .getElementById("chkFillPassword")
+  .addEventListener("click", checkPwVisibility);
 
 //for test puropses
 if (!chrome.storage) {
   chrome.storage = {
     sync: {
-      get: function (item, fn) { fn(item); },
-      set: function (item, fn) { fn(item); },
+      get: function(item, fn) {
+        fn(item);
+      },
+      set: function(item, fn) {
+        fn(item);
+      }
     }
-  }
+  };
 }
